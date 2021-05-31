ba9f19482dc0ec0434727867e66c0638b29d5f3f
Radiantly <buildingsomethingfun@gmail.com>
2021-05-31 21:36:45 +0530

    Fix bug when hiding 32-bit applications

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -16,16 +16,18 @@ using std::cerr;
 using std::endl;
 
 #ifdef _WIN64
-const auto iAm64bit = true;
-const auto hideDllName = L"./Hide.dll";
-const auto unhideDllName = L"./Unhide.dll";
+const bool iAm64bit = true;
+const std::wstring hideDllName{ L"./Hide.dll" };
+const std::wstring unhideDllName{ L"./Unhide.dll" };
 #else
-const auto iAm64bit = false;
-const auto hideDllName = L"./Hide_32bit.dll";
-const auto unhideDllName = L"./Unhide_32bit.dll";
+const bool iAm64bit = false;
+const std::wstring hideDllName{ L"./Hide_32bit.dll" };
+const std::wstring unhideDllName{ L"./Unhide_32bit.dll" };
 #endif
 
-const std::string title{ "  _____            _     _          _           _ \n"
+const std::wstring exeName{ L"./Invisiwind.exe" };
+const std::wstring exeName32{ L"./Invisiwind_32bit.exe" };
+const std::wstring title{ L"  _____            _     _          _           _ \n"
 "  \\_   \\_ ____   _(_)___(_)_      _(_)_ __   __| |\n"
 "   / /\\/ '_ \\ \\ / / / __| \\ \\ /\\ / / | '_ \\ / _` |\n"
 "/\\/ /_ | | | \\ V /| \\__ \\ |\\ V  V /| | | | | (_| |\n"
@@ -53,18 +55,6 @@ void showHelp(const wchar_t* argZero) {
 // Why do some functions have Ex? This is a new version of the function that has a different API to accomodate new features,
 // but has an Ex to prevent breaking old code.
 
-
-// From https://stackoverflow.com/questions/3828835/how-can-we-check-if-a-file-exists-or-not-using-win32-program
-// szPath - String Zero-terminated Path
-bool FileExists(LPCWSTR szPath)
-{
-	// https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-getfileattributesw
-	DWORD dwAttrib = GetFileAttributes(szPath);
-
-	return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
-		!(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
-}
-
 std::unordered_set<int> getPIDsFromProcName(std::wstring& searchTerm) {
 	std::unordered_set<int> pids;
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -88,7 +78,7 @@ std::map<std::wstring, std::unordered_set<int>> getProcList() {
 	std::map<std::wstring, std::unordered_set<int>> pList;
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (hSnapshot) {
-		PROCESSENTRY32 pe32;
+		PROCESSENTRY32 pe32{};
 		pe32.dwSize = sizeof(PROCESSENTRY32);
 		if (Process32First(hSnapshot, &pe32)) {
 			do {
@@ -108,41 +98,51 @@ bool isValidPID(std::wstring& arg) {
 	return true;
 }
 
+// From https://stackoverflow.com/questions/3828835/how-can-we-check-if-a-file-exists-or-not-using-win32-program
+// szPath - String Zero-terminated Path
+bool FileExists(std::wstring& filePath)
+{
+	// https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-getfileattributesw
+	DWORD dwAttrib = GetFileAttributes(filePath.c_str());
+
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+		!(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+std::wstring getFullFilePath(const std::wstring& filename) {
+	wchar_t fullPath[1024];
+	// DWORD GetFullPathNameW(
+	// 	LPCWSTR lpFileName,    - Relative path
+	// 	DWORD   nBufferLength, - Buffer size
+	// 	LPWSTR  lpBuffer,      - Buffer
+	// 	LPWSTR *lpFilePart     - Pointer to the filename part
+	// );
+	GetFullPathName(filename.c_str(), 256, fullPath, NULL);
+	std::wstring strFullPath{ fullPath };
+	if (!FileExists(strFullPath)) {
+		std::wcerr << "WARNING:" << strFullPath << " not found.";
+		return std::wstring{};
+	}
+	return strFullPath;
+};
+
 // LP  - Pointer
 // C   - Constant
 // T   - TCHAR (or W - WCHAR)
 // STR - String
 
-
 int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 {
-	auto getDllFullPath = [](const wchar_t* dllName) -> wchar_t* {
-		wchar_t dllFullPath[1024];
-		// DWORD GetFullPathNameW(
-		// 	LPCWSTR lpFileName,    - Relative path
-		// 	DWORD   nBufferLength, - Buffer size
-		// 	LPWSTR  lpBuffer,      - Buffer
-		// 	LPWSTR *lpFilePart     - Pointer to the filename part
-		// );
-		GetFullPathName(dllName, 256, dllFullPath, NULL);
-		if (!FileExists(dllFullPath)) {
-			std::wcerr << dllName << " not found.";
-			return nullptr;
-		}
-		return dllFullPath;
-	};
-
 	// Check if DLL exists and then store path
-	wchar_t *hideDllPath{ getDllFullPath(hideDllName) }, *unhideDllPath{ getDllFullPath(unhideDllName) };
+	std::wstring hideDllPath{ getFullFilePath(hideDllName) }, unhideDllPath{ getFullFilePath(unhideDllName) };
 
 	// Check if 32-bit version of the executable exists
-	TCHAR x86binPath[256];
-	GetFullPathName(L"./Invisiwind_32bit.exe", 256, x86binPath, NULL);
-	bool x86verExists = FileExists(x86binPath);
+	std::wstring x86binPath{ getFullFilePath(exeName32) };
+	bool x86verExists = x86binPath.length();
 
-	auto inject = [&](DWORD pid, wchar_t* dllFullPath) -> void {
-		if (!dllFullPath) return;
-		size_t dllPathLen = (wcslen(dllFullPath) + 1) * sizeof(TCHAR);
+	auto inject = [&](DWORD pid, std::wstring& dllFullPath) -> void {
+		if (!dllFullPath.length()) return;
+		size_t dllPathLen = (dllFullPath.length() + 1) * sizeof(wchar_t);
 		// HANDLE OpenProcess(
 		// 	DWORD dwDesiredAccess,
 		// 	BOOL  bInheritHandle,       - Whether child processes should inherit this handle
@@ -168,17 +168,19 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 					ZeroMemory(&pi, sizeof(pi));
 
 					std::wstring cliargs;
-					cliargs += L"Invisiwind_32bit.exe ";
+					cliargs += exeName32;
+					cliargs += L" ";
+					if (dllFullPath == unhideDllPath) cliargs += L"-u ";
 					cliargs += std::to_wstring(pid);
 
-					if (CreateProcess(x86binPath, &cliargs[0], NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+					if (CreateProcess(x86binPath.c_str(), &cliargs[0], NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
 						WaitForSingleObject(pi.hProcess, INFINITE);
 						CloseHandle(pi.hProcess);
 						CloseHandle(pi.hThread);
 					}
 					else cerr << "Unknown error occurred when trying to run 32-bit exe." << endl;
 				}
-				else cerr << "Cannot hide 32-bit process " << pid << " since Windoer_32bit.exe is missing." << endl;
+				else std::wcerr << "Cannot hide 32-bit process " << pid << " since " << exeName32 << " is missing." << endl;
 				CloseHandle(procHandle);
 				return;
 			}
@@ -188,6 +190,7 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 			// 	HMODULE *phModule       - Pointer to module handle if successful
 			// );
 			if (HMODULE libHandle; GetModuleHandleEx(0, L"kernel32.dll", &libHandle)) {
+				// cerr << "Kernel32.dll base address is " << libHandle << endl;
 				// FARPROC GetProcAddress(
 				// 	HMODULE hModule,    - Module handle
 				// 	LPCSTR  lpProcName  - Library/Variable you want the address of
@@ -209,7 +212,7 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 						// 	SIZE_T  nSize,                   - how much to write
 						// 	SIZE_T * lpNumberOfBytesWritten  - a pointer to store how many bytes were written
 						// );
-						if (WriteProcessMemory(procHandle, mem, dllFullPath, dllPathLen, NULL)) {
+						if (WriteProcessMemory(procHandle, mem, dllFullPath.c_str(), dllPathLen, NULL)) {
 							// HANDLE CreateRemoteThreadEx(
 							// 	HANDLE                       hProcess,
 							// 	LPSECURITY_ATTRIBUTES        lpThreadAttributes, - Security attributes
@@ -220,6 +223,8 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 							// 	LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList,
 							// 	LPDWORD                      lpThreadId
 							// );
+							// cerr << "Successfully wrote dllFullPath: ";
+							// std::wcerr << std::wstring(dllFullPath) << endl;
 							if (HANDLE remoteThread = CreateRemoteThreadEx(procHandle, NULL, 0, (LPTHREAD_START_ROUTINE)libAddr, mem, 0, NULL, NULL); remoteThread) {
 								// If we wanted to wait for the dll thread to return
 								// WaitForSingleObject(remoteThread, INFINITE);
@@ -242,8 +247,6 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 
 	if (argc > 1) {
 		bool hide = true;
-		wchar_t* dllPath{};
-		std::unordered_set<int> pids;
 		for (int i = 1; i < argc; i++) {
 			std::wstring arg{ argv[i] };
 			std::transform(arg.begin(), arg.end(), arg.begin(), ::towlower);
@@ -258,7 +261,7 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 				hide = false;
 			}
 			else if (isValidPID(arg)) {
-				inject(_wtoi(argv[i]), hide ? hideDllPath : unhideDllPath);
+				inject(stoi(arg), hide ? hideDllPath : unhideDllPath);
 			}
 			else {
 				auto pids = getPIDsFromProcName(arg);
@@ -276,9 +279,9 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 	}
 
 	// Interactive mode
-	std::cout << title << endl;
-	std::cout << "Hey I'm Invisiwind, here to make windows invisible to everyone but you ^_^" << endl;
-	std::cout << "Type `help` to get started." << endl;
+	std::wcout << title << endl;
+	std::wcout << "Hey I'm Invisiwind, here to make windows invisible to everyone but you ^_^" << endl;
+	std::wcout << "Type `help` to get started." << endl;
 
 	int enterPressed{};
 	while (true) {
@@ -290,7 +293,7 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 				cout << "Exiting .. Have a great day!";
 				return 0;
 			}
-			cout << "Type `list` to print a list of all processes or press Enter again to exit" << endl;
+			cout << "Press Enter again to exit" << endl;
 		}
 		else {
 			enterPressed = 0;
@@ -353,4 +356,3 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 
 	return 0;
 }
-
